17a3018ccbba462b99e52360b2bbf84a8b1ac731
Felipe Gomes <felipeferreiraag0@gmail.com>
2026-06-28 17:57:14 -0300

    Criando as patas estruturais

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// Incializando a sessão
+// session_start();
+
+// require_once __DIR__ . '/Config/Helpers.php';
+
+
+// spl_autoload_register(function ($class) {
+//     $baseDir = __DIR__ . '/';
+
+//     $class = str_replace('\\', '/', $class);
+
+//     $file = $baseDir . $class . '.php';
+
+//     if (file_exists($file)) {
+//         require_once $file;
+//     }
+// });
+
+// require_once __DIR__ . '/Router/Routes.php';
+// require_once __DIR__ . '/Core/Core.php';
+
+// $core = new Core();
+// $core->run($routes);
